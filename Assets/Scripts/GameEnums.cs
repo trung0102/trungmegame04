8522acc52d4557ebd605a0a918e96a227ade2cc6
trung0102 <trung.nguyen@hcmut.edu.vn>
2025-11-20 23:52:12 +0700

    Update Map

---
+++ Assets/Scripts/GameEnums.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public enum ResourceType
+{
+    Wood,
+    Brick,
+    Sheep,
+    Wheat,
+    Ore,
+    Desert
+}
+
+public enum BuildType
+{
+    Settlement,
+    City,
+    Road
+}
