86d8e62cc302469d0ef60bb60e6c774d0e8dc06d
trung0102 <trung.nguyen@hcmut.edu.vn>
2025-11-20 23:48:22 +0700

    Update attach title to EesProduction

---
+++ Assets/Scripts/CatanMap.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class CatanMap : MonoBehaviour
+{
+    public Tilemap tileMap;
+
+    public Tilemap numMap;
+    
+    public HexTileSet tileSet;
+    [SerializeField] private bool changeTileAtPosition = false;
+
+    protected List<ResProduction> productions = new List<ResProduction>();
+
+    [ContextMenu("Toggle Change Tile")]
+    private void ToggleChangeTile()
+    {
+        changeTileAtPosition = !changeTileAtPosition;
+    }
+    
+    void Start()
+    {
+        foreach (var kvp in TileDatabase.tileDict)
+        {
+            Vector3Int position = kvp.Key;
+            ResProductionInfo info = kvp.Value;
+
+            ResProduction res = gameObject.AddComponent<ResProduction>();
+            res.Initialize(position, info);
+            productions.Add(res);
+        }
+    }
+
+    void Update(){
+        if(Mouse.current.leftButton.wasPressedThisFrame){
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            mouseWorldPos.z = 0;
+
+
+            Vector3Int cellPos = tileMap.WorldToCell(mouseWorldPos); // Lấy ô hex
+            TileBase clickedTile = tileMap.GetTile(cellPos); // Lấy Tile hiện tại
+
+            if (clickedTile != null)
+            {   
+                // string info = "Khong co gi ca";
+                foreach (var product in productions)
+                {
+                    if(product.position == cellPos)
+                    {
+                        Debug.Log($"Clicked Hex: {cellPos}, Information: {product.PrintInfo()}");
+                    }
+                }
+                // TODO: Hiển thị UI hoặc info khác
+            }        
+            if(changeTileAtPosition) ChangeTileAtPosition(cellPos, ResourceType.Wood);    
+        }
+    }
+
+    public void ChangeTileAtPosition(Vector3Int position, ResourceType resource)
+    {   
+        foreach (var production in productions)
+        {
+            if(production.position == position)
+            {
+                production.ChangeTile(tileMap, tileSet, resource);
+                return;
+            }
+        }
+        // if (!tilemap.GetTile(position))
+        // {
+        Debug.Log($"Vị trí hiện tại không có tile");
+        return;
+        
+    }
+
+    public List<ResourceType> GetResourcesByDiceNumber(int diceNumber)
+    {   
+        List<ResourceType> listRes = new List<ResourceType>();
+        foreach (var prod in productions)
+        {
+            if(prod.information.numberToken == diceNumber)
+            {
+                listRes.Add(prod.information.resource);
+                Debug.Log($"Add: {prod.information.resource}");
+            }
+        }
+        return listRes;
+    }
+}
---
+++ Assets/Scripts/ResProduction.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+public class ResProduction : MonoBehaviour
+{
+    public Vector3Int position;
+    public ResProductionInfo information;
+
+    public void Initialize(Vector3Int position, ResProductionInfo information)
+    {
+        this.information = information;
+        this.position = position;
+    }
+    
+    public string PrintInfo()
+    {
+        return $"Position: {position} | Resource: {information.resource} | " +
+                  $"Token: {information.numberToken} | Blocked: {information.blocked}";
+    }
+
+    public void ChangeTile(Tilemap tilemap, HexTileSet tileSet, ResourceType resource)
+    {
+        information.resource = resource;
+        TileBase newTile = tileSet.GetTile(resource);
+        tilemap.SetTile(position, newTile);
+        tilemap.RefreshTile(position);
+        Debug.Log($"Đã chuyển tile tại: {position}, Tile Name: {newTile.name}");
+    }
+}
+
+public struct ResProductionInfo
+{
+    public ResourceType resource;
+
+    public int numberToken;
+
+    public bool blocked;
+
+    public ResProductionInfo(ResourceType resource, int numberToken, bool blocked = false)
+    {
+        this.resource = resource;
+        this.numberToken = numberToken;
+        this.blocked = blocked;
+    }
+}
+
+public static class TileDatabase
+{
+    public static Dictionary<Vector3Int, ResProductionInfo> tileDict = new Dictionary<Vector3Int, ResProductionInfo>()
+    {
+        { new Vector3Int(0, 2, 0), new ResProductionInfo(ResourceType.Brick, 10) },
+        { new Vector3Int(1, 2, 0), new ResProductionInfo(ResourceType.Wood, 3) },
+        { new Vector3Int(2, 2, 0), new ResProductionInfo(ResourceType.Ore, 4) },
+        { new Vector3Int(-1, 1, 0), new ResProductionInfo(ResourceType.Wood, 2) },
+        { new Vector3Int(0, 1, 0), new ResProductionInfo(ResourceType.Ore, 5) },
+        { new Vector3Int(1, 1, 0), new ResProductionInfo(ResourceType.Wheat, 6) },
+        { new Vector3Int(2, 1, 0), new ResProductionInfo(ResourceType.Sheep, 9) },
+        { new Vector3Int(-1, 0, 0), new ResProductionInfo(ResourceType.Sheep, 8) },
+        { new Vector3Int(0, 0, 0), new ResProductionInfo(ResourceType.Brick, 9) },
+        { new Vector3Int(1, 0, 0), new ResProductionInfo(ResourceType.Sheep, 3) },
+        { new Vector3Int(2, 0, 0), new ResProductionInfo(ResourceType.Desert, 0) },
+        { new Vector3Int(3, 0, 0), new ResProductionInfo(ResourceType.Wood, 6) },
+        { new Vector3Int(-1, -1, 0), new ResProductionInfo(ResourceType.Wheat, 12) },
+        { new Vector3Int(0, -1, 0), new ResProductionInfo(ResourceType.Wood, 8) },
+        { new Vector3Int(1, -1, 0), new ResProductionInfo(ResourceType.Ore, 11) },
+        { new Vector3Int(2, -1, 0), new ResProductionInfo(ResourceType.Wheat, 4) },
+        { new Vector3Int(0, -2, 0), new ResProductionInfo(ResourceType.Brick, 11) },
+        { new Vector3Int(1, -2, 0), new ResProductionInfo(ResourceType.Wheat, 10) },
+        { new Vector3Int(2, -2, 0), new ResProductionInfo(ResourceType.Sheep, 5) }
+    };
+}
